2d37ec1b47eff3aae3472f306fc9c04c34a2cc0d
jpedrocr <jpedrocr@gmail.com>
2016-10-28 19:49:42 +0100

    API Optimization

--- routes/api.php
+++ routes/api.php
@@ -16,19 +16,6 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-// App\Post::where('slug', 'n0')->with('elements.content')->first()->toArray()
-Route::get('post/{post_slug}', function ($post_slug) {
-  $with = [
-    'inicio-calendario' => 'elements.content.games.team.name',
-  ];
-  return response(
-    Post::where('slug', $post_slug)
-      ->with(array_get($with, $post_slug, 'elements.content'))
-      ->first()
-      ->toJson()
-  );
-});
-// App\Page::where('slug', 'galeria')->with('posts.elements.content')->first()->toArray()
 Route::get('page/{page_slug}', function ($page_slug) {
   $with = [
     'sdc-orgaos' => 'posts.elements.content.board_members.person.name',
@@ -41,64 +28,35 @@ Route::get('page/{page_slug}', function ($page_slug) {
     'galeria' => 'posts.elements.content.posts.elements.content',
   ];
   return response(
-    Page::where('slug', $page_slug)
-      ->with(array_get($with, $page_slug, 'posts.elements.content'))
-      ->with()
-      ->first()
-      ->toJson()
+    preg_replace('/,\s*"[^"]+":null|"[^"]+":null,?/', '',
+      Page::where('slug', $page_slug)
+        ->with(array_get($with, $page_slug, 'posts.elements.content'))
+        ->first()
+        ->toJson()
+    )
   );
 });
-
-
-
-
-
-Route::get('board-members', function () {
+Route::get('post/{post_slug}', function ($post_slug) {
+  $with = [
+    'inicio-calendario' => 'elements.content.games.team.name',
+  ];
   return response(
-    Club::with('boards.board_members.person.name')
-      ->first()
-      ->toJson()
+    preg_replace('/,\s*"[^"]+":null|"[^"]+":null,?/', '',
+      Post::where('slug', $post_slug)
+        ->with(array_get($with, $post_slug, 'elements.content'))
+        ->first()
+        ->toJson()
+    )
   );
 });
-
-Route::get('relevant-events', function () {
-  return response(
-    Club::with('relevant_events')
-      ->first()
-      ->toJson()
-  );
-});
-
-Route::get('entities', function () {
-  return response(
-    Club::with('sponsors.name', 'partners.name')
-      ->first()
-      ->toJson()
-  );
-});
-
-Route::get('teams', function () {
-  return response(
-    Season::with('teams.name')
-      ->get()
-      ->toJson()
-  );
-});
-
-Route::get('contacts', function () {
-  return response(
-    Club::with('name', 'contacts.contact_details', 'schedules', 'location')
-    ->first()
-    ->toJson()
-  );
-});
-
 Route::get('footer', function () {
   return response(
-    Club::with('name', 'contacts.contact_details')
-      ->first()
-      ->makeVisible(['description0', 'description1', 'description2', 'nipc'])
-      ->toJson()
+    preg_replace('/,\s*"[^"]+":null|"[^"]+":null,?/', '',
+      Club::with('name', 'contacts.contact_details')
+        ->first()
+        ->makeVisible(['description0', 'description1', 'description2', 'nipc'])
+        ->toJson()
+    )
   );
 });
 
